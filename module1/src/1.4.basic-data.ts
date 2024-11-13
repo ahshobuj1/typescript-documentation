272a460ce7faaba7d536620e7537e1ec6fddbebe
A H Shobuj <ahshobuj09@gmail.com>
2024-11-14 00:10:18 +0600

    Basic data type : Primitive and Non-Primitive data types

---
+++ module1/src/1.4.basic-data.ts
@@ -0,0 +1,29 @@
+// ? Primitive Data -> string, number, boolean, undefined, null, symbol
+// ? Non-Primitive Data(Reference Type) -> Array, Object, Tuple
+
+//* number
+let age: number = 12;
+
+//* string
+let names: string = 'a h';
+
+//* boolean
+let isVerified: boolean = true;
+
+//* undefined
+let X: undefined = undefined;
+
+//* null
+let Y: null = null;
+
+//? Non-Primitive (Reference Type Data)
+
+//* array
+let friends: string[] = ['Kevin De', 'De Maria'];
+let ages: number[] = [12, 33, 23];
+ages.push(24);
+
+//* tuple
+let students: [string, number, boolean] = ['Butler', 22, true];
+
+//* object
